2e30e2c9807770994f88309a236a574f5aa62a56
Rolando Lopantzi <rlopantzi@gmail.com>
2020-02-04 15:03:42 -0800

    init commit

---
+++ Boxing_unbox/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_unbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            
+            object BoxedData = "This is clearly a string";
+            if(BoxedData is int){
+                System.Console.WriteLine("I guess we have an integer value in this box?");
+            } if(BoxedData is string){
+                System.Console.WriteLine("It is a string in the box");
+            }
+            object ActuallyString = "a string";
+            string ExplicitString = ActuallyString as string;
+
+            System.Console.WriteLine(ActuallyString);
+            System.Console.WriteLine(ExplicitString);
+
+            // int ExplicitInt = ActuallyString as int:
+            // System.Console.WriteLine(ExplicitInt);
+
+            List<object> Test = new List<object>();
+            Test.Add(7);
+            Test.Add(28);
+            Test.Add(-1);
+            Test.Add(true);
+            Test.Add("chair");
+            foreach(object item in Test){
+                System.Console.WriteLine($"- {item}");
+            }
+            int sum = 0;
+            foreach(int item in Test){
+                sum += item;
+                System.Console.WriteLine(sum);
+            }
+
+        } 
+    }
+}
