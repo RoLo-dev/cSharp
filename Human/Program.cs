7308372975a80092decde140fc642ffdde50ab42
Rolando Lopantzi <rlopantzi@gmail.com>
2020-02-05 13:29:41 -0800

    init commit

---
+++ Human/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+
+    class Human{
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int Health;
+
+        public int health{
+            get{ return health; }
+        }
+        public Human(string name){
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            Health = 100;
+        }
+        public Human(string name, int strength, int intelligence, int dexterity, int hp)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = hp;
+        }
+        public int Attack(Human target){
+            int dmg = Strength * 5;
+            target.Health -= dmg;
+            return target.Health;
+        }
+    }
+}
