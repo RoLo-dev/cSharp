8acd876e8655b4c69c8ab8da2675984084ef9d43
Rolando Lopantzi <rlopantzi@gmail.com>
2020-02-08 22:52:56 -0800

    init commit

---
+++ ModelForms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ModelForms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
